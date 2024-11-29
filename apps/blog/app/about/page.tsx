3cfec875dd96cace8cd975aabecc06fab912ba9d
timDeHof <ttdehof@gmail.com>
2024-11-28 22:02:44 -0500

    initial commit

---
+++ apps/blog/app/about/page.tsx
@@ -0,0 +1,42 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/avatar";
+import { siteConfig } from "@/config/site";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "About Me",
+	description: "Information about me",
+};
+
+export default async function AboutPage() {
+	return (
+		<div className='container max-w-6xl py-6 lg:py-10'>
+			<div className='flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8'>
+				<div className='flex-1 space-x-4'>
+					<h1 className='inline-block font-black text-4xl lg:text-5xl'>
+						About Me
+					</h1>
+				</div>
+			</div>
+			<hr className='my-8' />
+			<div className='flex flex-col md:flex-row gap-8 items-center md:items-start'>
+				<div className='min-w-48 max-w-48 flex flex-col gap-2'>
+					<Avatar className='h-48 w-48'>
+						<AvatarImage src='/avatar-tim.png' alt={siteConfig.author} />
+						<AvatarFallback>TD</AvatarFallback>
+					</Avatar>
+					<h2 className='text-2xl font-bold text-center break-words'>
+						{siteConfig.author}
+					</h2>
+					<p className='text-muted-foreground text-center break-words'>
+						Software Developer
+					</p>
+				</div>
+				<p className='text-muted-foreground text-lg py-4'>
+					As a full-stack developer, I have a passion for creating dynamic and
+					responsive websites. I am a self-taught developer with a strong
+					background in both frontend and backend development.
+				</p>
+			</div>
+		</div>
+	);
+}
