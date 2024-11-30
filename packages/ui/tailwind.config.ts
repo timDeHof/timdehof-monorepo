e575572c0eb04be2987c7a89d0e2be87f65f60bc
timDeHof <ttdehof@gmail.com>
2024-11-29 22:10:07 -0500

    feat(ui): add tailwindcss typography plugin to enhance text styling The tailwindcss typography plugin has been added to the project to provide more options for text styling and formatting. This will allow for better control over typography in the UI, improving the overall look and feel of the application.

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 import tailwindcssAnimate from "tailwindcss-animate";
+import tailwindcssTypography from "@tailwindcss/typography";
 
 
 const config = {
@@ -81,7 +82,7 @@ const config = {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [tailwindcssAnimate, tailwindcssTypography],
 } satisfies Config;
 
 export default config;
